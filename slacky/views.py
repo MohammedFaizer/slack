853d2b7f9cea992c52a8a9301ae94d0104813f33
Mohammed Faizer <zmd5zer@gmail.com>
2024-06-20 14:17:43 +0530

    second commit

--- slacky/views.py
+++ slacky/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['POST'])
+@api_view(['GET','POST'])
 def hello_world(request):
     print(request.data)
     return Response({"message": "Hello, world!"})
