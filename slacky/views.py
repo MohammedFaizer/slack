94d5e0c024eb0814938c480bd215bf5877b5482c
Mohammed Faizer <zmd5zer@gmail.com>
2024-06-20 15:07:40 +0530

    second commit

--- slacky/views.py
+++ slacky/views.py
@@ -1,67 +1,74 @@
-from django.shortcuts import render
-
+import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'
+
 @api_view(['POST'])
 def hello_world(request):
-    print(request.data)
-    return Response({
-	"blocks": [
-		{
-			"type": "header",
-			"text": {
-				"type": "plain_text",
-				"text": "New request",
-				"emoji": true
-			}
-		},
-		{
-			"type": "section",
-			"fields": [
-				{
-					"type": "mrkdwn",
-					"text": "*Type:*\nPaid Time Off"
-				},
-				{
-					"type": "mrkdwn",
-					"text": "*Created by:*\n<example.com|Fred Enriquez>"
-				}
-			]
-		},
-		{
-			"type": "section",
-			"fields": [
-				{
-					"type": "mrkdwn",
-					"text": "*When:*\nAug 10 - Aug 13"
-				}
-			]
-		},
-		{
-			"type": "actions",
-			"elements": [
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"emoji": true,
-						"text": "Approve"
-					},
-					"style": "primary",
-					"value": "click_me_123"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"emoji": true,
-						"text": "Reject"
-					},
-					"style": "danger",
-					"value": "click_me_123"
-				}
-			]
-		}
-	]
-})
+    slack_message = {
+        "blocks": [
+            {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Welcome To the todo app",
+                    "emoji": True
+                }
+            },
+            {
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Type:*\nPaid Time Off"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Created by:*\n<example.com|Fred Enriquez>"
+                    }
+                ]
+            },
+            {
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "*When:*\nAug 10 - Aug 13"
+                    }
+                ]
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": True,
+                            "text": "Approve"
+                        },
+                        "style": "primary",
+                        "value": "click_me_123"
+                    },
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": True,
+                            "text": "Reject"
+                        },
+                        "style": "danger",
+                        "value": "click_me_123"
+                    }
+                ]
+            }
+        ]
+    }
+
+    response = requests.post(SLACK_WEBHOOK_URL, json=slack_message)
+
+    if response.status_code != 200:
+        return Response({'error': 'Request to Slack returned an error %s, the response is:\n%s' % (response.status_code, response.text)}, status=response.status_code)
+
+    return Response({'message': 'Notification sent to Slack successfully!'}, status=200)
