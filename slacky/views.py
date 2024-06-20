e6ab0d38b769280f56a7be46f1d2f9d71447b414
Mohammed Faizer Z <80749640+MohammedFaizer@users.noreply.github.com>
2024-06-20 15:59:15 +0530

    Update views.py

--- slacky/views.py
+++ slacky/views.py
@@ -2,7 +2,7 @@ import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'
+SLACK_WEBHOOK_URL = ''
 
 @api_view(['POST'])
 def hello_world(request):
