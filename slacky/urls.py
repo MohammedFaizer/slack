ef634816e303419c45d9102ace0aabe4541d8e64
Mohammed Faizer <zmd5zer@gmail.com>
2024-06-20 12:40:23 +0530

    first commit

---
+++ slacky/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path,include
+from .views import hello_world
+urlpatterns = [
+path('hello_world/', hello_world , name='hello_world')
+]
---
+++ slacky/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['POST'])
+def hello_world(request):
+    print(request.data)
+    return Response({"message": "Hello, world!"})
